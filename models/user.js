14c985cb90798d2ec48eeb79b3c0c16d2f11cb10
Sput <paulknick@gmail.com>
2021-05-13 12:59:19 -0400

    add user model checks

--- models/user.js
+++ models/user.js
@@ -1,30 +1,4 @@
-
-// 'use strict';
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class user extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   };
-//   user.init({
-//     name: DataTypes.STRING,
-//     email: DataTypes.STRING,
-//     password: DataTypes.STRING
-//   }, {
-//     sequelize,
-//     modelName: 'user',
-//   });
-//   return user;
-// };
-
+const bcrypt = require('bcrypt');
 
 const {
   Model
@@ -72,5 +46,56 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'user',
   });
 
+  user.addHook('beforeCreate', (pendingUser) => { // pendingUser is user object that gets passed to DB
+    // Bcrypt is going to hash the password
+    let hash = bcrypt.hashSync(pendingUser.password, 12); // hash 12 times
+    pendingUser.password = hash; // this will go to the DB
+});  
+
+ // Check the password on Sign-In and compare it to the hashed password in the DB
+ user.prototype.validPassword = function(typedPassword) {
+  let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
+
+  return isCorrectPassword;
+}
+
+// return an object from the database of the user without the encrypted password
+user.prototype.toJSON = function() {
+  let userData = this.get(); 
+  delete userData.password; // it doesn't delete password from database, only removes it. 
+  
+  return userData;
+}
+
+
   return user; // add functions above 
 };
+
+
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class user extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   user.init({
+//     name: DataTypes.STRING,
+//     email: DataTypes.STRING,
+//     password: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'user',
+//   });
+//   return user;
+// };
+
+
