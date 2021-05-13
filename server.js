476f086266e4f6755051eb1c996bd1790776b6f4
Sput <paulknick@gmail.com>
2021-05-13 15:06:51 -0400

    isLoggedIn: add middleware and import to server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const layouts = require('express-ejs-layouts');
 const app = express();
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('./config/ppConfig');
+const isLoggedIn = require('./middleware/isLoggedIn');
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
 console.log(SECRET_SESSION);
@@ -14,6 +16,10 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+//PP middlware
+app.use(passport.initialize());      // Initialize passport
+app.use(passport.session());         // Add a session
+
 
 app.use(session({
   secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
